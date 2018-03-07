b49ef95469fc5686dd38fd1e12be7f19bb3224a7
ikutarian <saitou0217@outlook.com>
2018-03-07 16:01:02 +0800

    1) 补充部分API文档 2) 显示最热模块的数据

--- pages/hot/hot.js
+++ pages/hot/hot.js
@@ -5,62 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
-  },
+    latest_topics: [
+      {
+        "id": 435431,
+        "title": "苹果官方技术顾问非法窃取用户个人信息和资料并敲诈勒索的事件",
+        "replies": 175,
+        "member": {
+          "id": 289264,
+          "username": "orangeade",
+          "avatar_normal": "//cdn.v2ex.com/avatar/34db/1a81/289264_normal.png?m=1517653170"
+        },
+        "node": {
+          "title": "Apple"
+        }
+      },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+      {
+        "id": 435471,
+        "title": "求助：试用期第三个月，我要离职，老板说招不到新人就不让我走，我想走可很害怕，我该怎么办。",
+        "replies": 155,
+        "member": {
+          "id": 294224,
+          "username": "hubahuba",
+          "avatar_normal": "//cdn.v2ex.com/avatar/3f84/53c5/294224_normal.png?m=1520311966"
+        },
+        "node": {
+          "title": "程序员"
+        }
+      },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+      {
+        "id": 435321,
+        "title": "你们上车腾讯云，除了吃灰还能干哈？",
+        "replies": 145,
+        "member": {
+          "id": 70908,
+          "username": "Chigogo",
+          "avatar_normal": "//cdn.v2ex.com/avatar/db9b/45c8/70908_normal.png?m=1408788473"
+        },
+        "node": {
+          "title": "服务器"
+        }
+      }
+    ]
   }
 })
